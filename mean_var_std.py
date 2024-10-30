863fbde6b6d2241227da95f5e4b70903334b45f6
Ange Lavialle <ange.lavialle@student-cs.fr>
2024-10-30 17:17:55 +0000

    Implement calculate function for mean, variance, and standard deviation

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,15 @@
 import numpy as np
 
 def calculate(list):
-
-
-
-
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
+    tab = np.array(list)
+    tab = np.reshape(tab, (3,3))
+    calculations = {}
+    calculations['mean'] = [np.mean(tab, axis = 0).tolist(), np.mean(tab, axis = 1).tolist(), np.mean(tab).tolist()]
+    calculations['variance'] = [np.var(tab, axis = 0).tolist(), np.var(tab, axis = 1).tolist(), np.var(tab).tolist()]
+    calculations['standard deviation'] = [np.std(tab, axis = 0).tolist(), np.std(tab, axis = 1).tolist(), np.std(tab).tolist()]
+    calculations['max'] = [np.max(tab, axis = 0).tolist(), np.max(tab, axis = 1).tolist(), np.max(tab).tolist()]
+    calculations['min'] = [np.min(tab, axis = 0).tolist(), np.min(tab, axis = 1).tolist(), np.min(tab).tolist()]
+    calculations['sum'] = [np.sum(tab, axis = 0).tolist(), np.sum(tab, axis = 1).tolist(), np.sum(tab).tolist()]
     return calculations
